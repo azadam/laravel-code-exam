57d6a19649c9acc70e43b997fa00b0dcb6d0520c
Adam Hooker <adamh@rodanandfields.com>
2017-03-08 15:52:19 -0800

    Adding routes to handle the various crud ops; not being strict with inputs other than sanitization, and leveraging eloquent to abstract out the other details

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use App\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +15,36 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('/task', function () {
+    $task = new App\Task();
+    $task->task_completed = 0;
+    if (Input::has('task_completed')) {
+        $task->task_completed = Input::get('task_completed') == '1' ? 1 : 0;
+    }
+    $task->task_description = Input::get('task_description', '');
+    $task->save();
+    return response()->json($task);
+});
+
+Route::get('/task', function () {
+    $tasks = App\Task::all();
+    return response()->json($tasks);
+});
+
+Route::put('/task/{id}', function ($id) {
+    $task = App\Task::findOrFail($id);
+    if (Input::has('task_description')) {
+        $task->task_description = Input::get('task_description');
+    }
+    if (Input::has('task_completed')) {
+        $task->task_completed = Input::get('task_completed') == '1' ? 1 : 0;
+    }
+    $task->save();
+    return response()->json($task);
+});
+
+Route::delete('/task/{id}', function ($id) {
+    $task = App\Task::findOrFail($id);
+    $task->delete();
+    return '';
+});
