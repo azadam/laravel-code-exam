fa03c9244e28a1efd823d7748132f7f23a65097f
Adam Hooker <adamh@rodanandfields.com>
2017-03-08 15:38:29 -0800

    Plugging in the base task app html/css and stub file to contain js

---
+++ resources/views/taskapp.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TahDah</title>
+    <link rel="stylesheet" href="./css/base.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="./js/taskapp.js"></script>
+  </head>
+  <body>
+    <section id="todoapp">
+      <header id="header">
+        <h1>TahDah</h1>
+        <input id="new-todo" placeholder="What needs to be done?" autofocus>
+      </header>
+      <section id="main">
+        <ul id="todo-list">
+          <li>
+            <div class="view">
+              <input class="toggle" type="checkbox">
+              <label>Foo</label>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <footer id="footer"></footer>
+    </section>
+    <footer id="info">
+      <p>HTML and CSS shamelessly ripped from <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+  </body>
+</html>
